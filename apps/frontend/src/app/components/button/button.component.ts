9a9c48503cd00e5d940510b06b15076d831f20d2
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-07-08 20:22:42 +0200

    passage du bouton ajouter en svg pour le responsive

--- apps/frontend/src/app/components/button/button.component.ts
+++ apps/frontend/src/app/components/button/button.component.ts
@@ -5,12 +5,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: `<button
-    class="flex justify-center items-center text-3xl lg:text-6xl
+    class="flex justify-center items-center
   text-JobTracker-gray bg-JobTracker-side rounded-full
-   w-9 h-9 lg:w-18 lg:h-18 cursor-pointer
+   w-12 h-12 cursor-pointer
   hover:bg-JobTracker-side-hover fixed bottom-8 right-2 md:right-5"
   >
-    <p class="mb-0.5 lg:mb-1.5">+</p>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+    >
+      <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z" />
+    </svg>
   </button>`,
 })
 export class ButtonComponent {}
