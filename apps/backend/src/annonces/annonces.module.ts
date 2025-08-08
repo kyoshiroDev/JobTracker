9b8c495801e05432a168046e02a18445b0e41b42
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-08-08 07:41:01 +0200

    gestion crud des annonces ok

--- apps/backend/src/annonces/annonces.module.ts
+++ apps/backend/src/annonces/annonces.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnnoncesService } from './annonces.service';
 import { AnnoncesController } from './annonces.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AnnoncesController],
   providers: [AnnoncesService],
 })
--- apps/backend/src/annonces/dto/create-annonces.dto.ts
+++ apps/backend/src/annonces/dto/create-annonces.dto.ts
@@ -1,1 +1,58 @@
-export class CreateAnnoncesDto {}
+import { IsString, IsOptional, IsEmail, IsUrl, IsUUID } from 'class-validator';
+
+export class CreateAnnonceDto {
+  @IsString()
+  job!: string;
+
+  @IsString()
+  contract_type!: string;
+
+  @IsString()
+  status!: string;
+
+  @IsOptional()
+  @IsString()
+  about?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  skills?: string;
+
+  @IsOptional()
+  @IsString()
+  benefits?: string;
+
+  @IsOptional()
+  @IsString()
+  salary?: string;
+
+  @IsOptional()
+  @IsUrl()
+  annonce_link?: string;
+
+  @IsString()
+  work_mode!: string;
+
+  @IsOptional()
+  @IsUUID()
+  user_id!: string;
+
+  @IsString()
+  company_name!: string;
+
+  @IsString()
+  company_city!: string;
+
+  @IsOptional()
+  @IsString()
+  company_phone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  company_email?: string;
+
+}
