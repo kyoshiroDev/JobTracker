084b9a92066d4b87322ca783c2f8ac2e9b47d2f6
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-08-13 19:31:07 +0200

    fix(route):modification du fichier de route avec suppression des children signin et signup

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,22 +1,13 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from '../auth/auth.component';
-import { SigningComponent } from '../auth/signing/signing.component';
-import { SignupComponent } from '../auth/signup/signup.component';
 import { NotFoundComponent } from './not-found.component';
+import { DashboardComponent } from '../dashboard/dashboard-page.component';
 
 
 export const routes: Routes = [
+  { path: '', component: AuthComponent },
   {
-    path: '',
-    component: AuthComponent,
-    children: [
-      { path: '', pathMatch: 'full', redirectTo: 'sign-in' },
-      { path: 'sign-in', component: SigningComponent }, // Non lazy
-      { path: 'sign-up', component: SignupComponent }, // Non lazy
-    ],
-  },
-  {
-    path: 'dashboard',
+    path: 'dashboard', component: DashboardComponent,
     loadChildren: () =>
       import('../annonce/annonce-liste/annonce-liste-page.component').then((m) => m.AnnonceListePageComponent),
   },
