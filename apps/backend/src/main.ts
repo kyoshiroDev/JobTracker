47d6a05424f3620cd0bd7676169e0fe8ca117b6d
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-08-16 08:27:16 +0200

    fix(CORS): Essaie de l'application en ligne avec aucune restriction d'adresse

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,15 +5,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS d'abord
-  app.enableCors({
-    origin: '*',
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // mets à false si tu n'utilises pas de cookies
-  });
-
-  // pipes, etc.
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
