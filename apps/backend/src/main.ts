e61b88ef8990f3f2980faae6567f8b2c41c79217
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-08-18 21:55:19 +0200

    chore: ajout de allowedHeaders

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,8 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://jobtrakerv2.netlify.app'],
+    origin: ['http://localhost:4200', 'https://jobtrakerv2.netlify.app/'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
