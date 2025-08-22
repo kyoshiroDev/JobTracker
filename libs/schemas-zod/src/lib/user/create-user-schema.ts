1ccce7e3d4c4b0c9b89a62f1cea77afaabeb1df8
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-08-22 19:18:34 +0200

    feature(Auth): partie enregistrement fonctionnel avec redirection vers dashboard une fois enregistrer

--- libs/schemas-zod/src/lib/user/create-user-schema.ts
+++ libs/schemas-zod/src/lib/user/create-user-schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const createUserSchema = z.object({
   username: z.string().nonempty('Le nom est obligatoire'),
   email: z.string().email('Email invalide').nonempty("L'email est obligatoire"),
-  password: z.string().min(8, 'Mot de passe trop court').nonempty('Le mot de passe est obligatoire'),
+  password: z.string().min(1, 'Mot de passe trop court').nonempty('Le mot de passe est obligatoire'),
   createdAt: z.coerce.date().optional(),
 }).strict();
 
