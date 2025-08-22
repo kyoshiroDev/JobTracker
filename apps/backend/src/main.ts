8c0f6fff11f77898f5c0c4267a3fc7874de0d0e3
Gregory Tahir <tahir.gregory.dev@gmail.com>
2025-08-22 19:12:07 +0200

    fix(CORS): correction de origin dans main.ts suppression de / à la fin de l'adresse netlify

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://jobtrakerv2.netlify.app/'],
+    origin: ['http://localhost:4200', 'https://jobtrakerv2.netlify.app'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', true, 'X-Requested-With'],
     credentials: true,
