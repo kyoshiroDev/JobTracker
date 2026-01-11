a1f03ef50364bc33c20dc8f1546c7219946b63ce
Gregory Tahir <tahir.gregory.dev@gmail.com>
2026-01-11 13:43:35 +0100

    chore: remove candidature zod schemas lib

--- libs/schemas-zod/src/index.ts
+++ libs/schemas-zod/src/index.ts
@@ -4,7 +4,4 @@ export * from './lib/auth/login-user.schema';
 // user
 export * from './lib/user/create-user-schema';
 
-// candidatures
-export * from './lib/annonce/create-annonce-schema';
-export * from './lib/annonce/update-annonce-schema';
-export * from './lib/annonce/annonce.enums';
+// candidature
